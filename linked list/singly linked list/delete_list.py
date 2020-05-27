8406304cae10ac4c1f7c5728deb27b56a170aad8
akshay bogar <akshaymbogar0099@gmail.com>
2020-05-27 22:32:12 +0530

    program to delete linked list

---
+++ linked list/singly linked list/delete_list.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Insert nodes at the beginning of linked list
+    def push(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+        return
+
+    def delete_list(self):
+        self.head = None
+        return
+
+    #Utility method to print list
+    def print_list(self):
+        node = self.head
+        while node:
+            print(node.data)
+            node = node.next
+
+if __name__ == '__main__':
+    linked_list = LinkedList()
+    linked_list.push(4)
+    linked_list.push(3)
+    linked_list.push(2)
+    linked_list.push(1)
+    linked_list.print_list()
+    linked_list.delete_list()
+    linked_list.print_list()

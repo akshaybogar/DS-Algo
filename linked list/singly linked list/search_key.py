7182c42b93c43ae4e6b610a9bf6ba73ce941c5ae
akshay bogar <akshaymbogar0099@gmail.com>
2020-05-27 22:46:28 +0530

    program to search an element in linked list

---
+++ linked list/singly linked list/search_key.py
@@ -0,0 +1,30 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Insert nodes at the beginning of linked list
+    def push(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+        return
+
+    def search_key(self, node, key):
+        if not node:
+            return False
+        elif node.data == key:
+            return True
+        return self.search_key(node.next, key)
+
+if __name__ == '__main__':
+    linked_list = LinkedList()
+    linked_list.push(4)
+    linked_list.push(3)
+    linked_list.push(2)
+    linked_list.push(1)
+    print(linked_list.search_key(linked_list.head, 2))

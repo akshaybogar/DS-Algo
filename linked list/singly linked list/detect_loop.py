52e421dfc4a275b47e8c1458769006df7ff41394
akshay bogar <akshaymbogar0099@gmail.com>
2020-06-02 22:28:21 +0530

    program to detect loop in linked list

---
+++ linked list/singly linked list/detect_loop.py
@@ -0,0 +1,42 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # Insert nodes at the beginning of linked list
+    def push(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+        return
+
+    def create_loop(self):
+        if not self.head:
+            return
+        # Make sure the the linked list contains atleast 5 elements
+        self.head.next.next.next.next.next = self.head.next
+        return
+
+    def detect_loop(self):
+        slow, fast = self.head, self.head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                print('loop exists')
+                return
+        print('no loop')
+
+if __name__ == '__main__':
+    linked_list = LinkedList()
+    linked_list.push(5)
+    linked_list.push(4)
+    linked_list.push(3)
+    linked_list.push(2)
+    linked_list.push(1)
+    linked_list.create_loop()
+    linked_list.detect_loop()
